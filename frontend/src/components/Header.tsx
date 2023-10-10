e37545d4777cc5435ea75172b1dd4f9f4b0b63c1
PhamDuy <vanduy342001@gmail.com>
2023-10-10 19:23:35 +0700

    Setup frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+        <nav>
+            <h1>hello</h1>
+            <h1>Login</h1>
+            <h1>Register</h1>
+        </nav>
+    )
+}
+export default Header;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home hello</h1>
+        </div>
+    )
+}
+export default HomePage
